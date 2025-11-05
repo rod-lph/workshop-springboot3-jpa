d655276ddb987b063c483993490d00ccb321e8be
Ródney Lúcio <rodney.lucioh@gmail.com>
2025-11-05 19:07:59 -0300

    Category

---
+++ spring/src/test/java/com/rodproject/spring/services/CategoryService.java
@@ -0,0 +1,29 @@
+package com.rodproject.spring.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.rodproject.spring.entities.Category;
+import com.rodproject.spring.entities.User;
+import com.rodproject.spring.repositories.CategoryRepository;
+import com.rodproject.spring.repositories.UserRepository;
+
+@Service
+public class CategoryService {
+	
+	@Autowired
+	private CategoryRepository repository;
+	
+	public List<Category> findAll() {
+		return repository.findAll();
+	}
+	
+	public Category findById(Long id) {
+		Optional<Category> obj = repository.findById(id);
+		return obj.get();
+	}
+
+}
