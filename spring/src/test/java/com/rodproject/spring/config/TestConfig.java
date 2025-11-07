a11452b6d093c75433a96230ed5050b2d38609a9
Ródney Lúcio <rodney.lucioh@gmail.com>
2025-11-07 11:02:51 -0300

    Payment, one-to-one association

--- spring/src/test/java/com/rodproject/spring/config/TestConfig.java
+++ spring/src/test/java/com/rodproject/spring/config/TestConfig.java
@@ -11,6 +11,7 @@ import org.springframework.context.annotation.Profile;
 import com.rodproject.spring.entities.Category;
 import com.rodproject.spring.entities.Order;
 import com.rodproject.spring.entities.OrderItem;
+import com.rodproject.spring.entities.Payment;
 import com.rodproject.spring.entities.Product;
 import com.rodproject.spring.entities.User;
 import com.rodproject.spring.entities.enums.OrderStatus;
@@ -81,6 +82,10 @@ public class TestConfig implements CommandLineRunner {
 		
 		orderItemRepository.saveAll(Arrays.asList(oi1, oi2, oi3, oi4));
 		
+		Payment pay1 = new Payment(null, Instant.parse("2019-06-20T21:53:07Z"), o1);
+		o1.setPayment(pay1);
+		
+		orderRepository.save(o1);
 		
 	}
 }
